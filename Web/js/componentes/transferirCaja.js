7fe64e9d3a8f1f9129c5a14eed53cd30b241efde
RichardLaptop <r_zapatac@hotmail.com>
2018-01-03 18:41:55 -0500

    caja

---
+++ Web/js/componentes/transferirCaja.js
@@ -0,0 +1,55 @@
+﻿Vue.component('tranferirCaja', {
+    props: ['origen', 'destino'],
+    created: function () {
+        var self = this;
+        $.get('./Cajadiario/ObtenerBoveda', {}, function (d) {
+            self.CajaOrigen = d;
+        });
+    },
+    data: function () {
+        return {
+            CajaOrigen: {},
+            CajaDestino: {}
+        }
+    },
+    template: `
+                <div>
+                    <div id="modalTransferenciaCaja" class="modal">
+                      <div class="modal-content">
+                          <div id="input-select" class="row">
+                                <div class="col s12 ">
+                                      <div class="input-field col s12 m6 l6">
+                                        <label>Caja Origen</label>
+                                        <select>
+                                          <option value="" disabled selected>Choose your option</option>
+                                          <option value="1">Option 1</option>
+                                          <option value="3">Option 3</option>
+                                        </select>
+    {{this.CajaOrigen}}
+                                      </div>
+                                      <div class="input-field col s12 m6 l6">
+                                        <label>Caja Destino</label>
+                                        <select>
+                                          <option value="" disabled selected>Choose your option</option>
+                                          <option value="1">Option 1</option>
+                                          <option value="3">Option 3</option>
+                                        </select>
+                                      </div>
+                                </div>
+                          </div>
+                          <div class ="row" >                                
+                                <div class="input-field col s6 right" >
+                                      <i class ="mdi-editor-attach-money prefix"></i>
+                                      <input id="txtImporteModal" type="text" class ="validate">
+                                      <label for="txtImporteModal" style="width:0">Importe</label>
+                                </div>
+                          </div>
+                      </div>
+                      <div class="modal-footer">
+                        <a href="#" class="btn waves-effect waves-red btn-flat modal-action modal-close">Cerrar</a>
+                        <a href="#" class="btn waves-effect waves-green btn-flat modal-action modal-close">Transferir</a>
+                      </div>
+                    </div>
+                </div>
+              `,
+});
